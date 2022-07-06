534de68b7c96e614ab9049a94561490509604686
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-07-06 23:46:19 +0600

    User details are shown

--- src/components/UserDetails/UserDetails.js
+++ src/components/UserDetails/UserDetails.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
 
 const UserDetails = () => {
+    const [users, setUsers] = useState([]);
+    const [reservations, setReservations] = useState([]);
+    useEffect(() => {
+      fetch("http://localhost:5000/users")
+        .then((res) => res.json())
+        .then((data) => setUsers(data));
+    }, []);
+    useEffect(() => {
+      fetch("http://localhost:5000/reservations")
+        .then((res) => res.json())
+        .then((data) => setReservations(data));
+    }, []);
+    const filteredReservation = (email) => {
+        const selectedReservations = reservations.filter(
+          (reservation) => reservation.email === email
+        );
+        return selectedReservations;
+      };
+     
     return (
         <div>
-         <h1>User Details</h1>   
+         <Table striped bordered hover className="my-5 container">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Users</th>
+            <th>Reservation Made</th>
+            <th>Number of Reservation</th>
+            <th>Reservations</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, count = 0) => (
+            <tr key={user._id}>
+              <td>{count + 1}</td>
+              <td>{user?.email}</td>
+              <td>
+                {filteredReservation(user?.email)[0]?.email ? (
+                  <span className="text-success fw-bold">Yes</span>
+                ) : (
+                  <span className="text-danger fw-bold">No</span>
+                )}
+              </td>
+             
+              <td>{filteredReservation(user?.email).length}</td>
+              <td>{filteredReservation(user?.email).map(reservation=><span>{reservation.roomType},</span>)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>  
         </div>
     );
 };
